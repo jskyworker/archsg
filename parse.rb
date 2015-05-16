430be73eaccbbf80765ee929772138d1fa166ffd
Jack Skyworker (home) <jskyworker@gmail.com>
2015-05-16 16:07:56 +0300

    main

--- parse.rb
+++ parse.rb
@@ -3,6 +3,35 @@ require 'rubygems'
 
 def main(parsed)
   puts 'Plain mode.'
+  groups_i = []
+  groups_o = []
+
+  parsed['SecurityGroups'].each do |i|
+    if i['IpPermissions']
+      i['IpPermissions'].each do |j|
+        j['IpRanges'].each do |l|
+          if l['CidrIp'] == '0.0.0.0/0'
+            groups_o.push("#{i['GroupName']}, #{j['IpProtocol']}, #{j['ToPort']}")
+          else
+            groups_i.push("#{l['CidrIp']}")
+          end
+        end
+      end
+    end
+  end
+
+  open('/home/jack/ruby-report1', 'w') do |f|
+    groups_o.sort.each do |k|
+      puts "#{k}"
+      f.puts "#{k}\n"
+    end
+    puts 'IP addresses with a special ACLs'
+    f.puts "\n\nIP addresses with a special ACLs\n"
+    groups_i.sort.uniq.each do |k|
+      puts "IP #{k}"
+      f.puts "#{k}\n"
+    end
+  end
 
 end
 
@@ -30,7 +59,7 @@ def cron(parsed)
 end
 
 
-file = open('/home/jack/allsgs.json')
+file = open('/home/jack/allsgs.json', 'r')
 json = file.read
 parsed = JSON.parse(json)
 
